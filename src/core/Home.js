06fcee8615a81f78eaba38e2ac917c66f97e48f6
BukunmiPeter <oluwabukunmiadeyemo99@gmail.com>
2022-04-26 05:52:13 +0100

    update

--- src/core/Home.js
+++ src/core/Home.js
@@ -5,8 +5,6 @@ import Card from "./Card";
 import Search from "./Search";
 import "./home.css";
 import Footer from "../core/Footer";
-// import "bootstrap/dist/css/bootstrap.css";
-// import Carousel from "react-bootstrap/Carousel";
 
 const Home = () => {
   const [productsBySell, setProductsBySell] = useState([]);
@@ -39,46 +37,17 @@ const Home = () => {
     loadProductsBySell();
   }, []);
 
-  // const Carousel = () => {
-  //   return (
-  //     <div style={{ display: "block", width: 700, padding: 30 }}>
-  //       <h4>React-Bootstrap Carousel Component</h4>
-  //       <Carousel>
-  //         <Carousel.Item interval={1500}>
-  //           <img
-  //             className="d-block w-100"
-  //             src="/bookstoreimages/carousel1.jpg"
-  //             alt="Image One"
-  //           />
-  //           <Carousel.Caption>
-  //             {/* <h3>Label for first slide</h3>
-  //             <p>Sample Text for Image One</p> */}
-  //           </Carousel.Caption>
-  //         </Carousel.Item>
-  //         <Carousel.Item interval={500}>
-  //           <img
-  //             className="d-block w-100"
-  //             src="/bookstoreimages/carousel2.jpg"
-  //             alt="Image Two"
-  //           />
-  //           <Carousel.Caption>
-  //             {/* <h3>Label for second slide</h3>
-  //             <p>Sample Text for Image Two</p> */}
-  //           </Carousel.Caption>
-  //         </Carousel.Item>
-  //       </Carousel>
-  //     </div>
-  //   );
-  // };
-
   return (
     <Layout title="" description="" className="home-layout">
       <Search />
-      {/* <Carousel /> */}
       <div className="image-div-container">
         <div className="home-image-div">
-          <h2 className="text-bg">Brightbooks.</h2>
-          <h6 className="text-bg2">Your desires are well delivered here.</h6>
+          <div className="textbg-div">
+            <h2 className="text-bg">Brightbooks.</h2>
+          </div>
+          <div className="text-bg-2-div">
+            <h6 className="text-bg2">Your desires are well delivered here.</h6>
+          </div>
         </div>
       </div>
       <div className="arrival-products">
